39b3191a37305a71b748c50cc7a4ce0bb18484fb
Zubair <developer@bairisland.com>
2024-03-16 01:54:35 -0700

    Adds first app

---
+++ My First App.swiftpm/ContentView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "hammer.fill")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("My First App")
+        }
+    }
+}
